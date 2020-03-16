79e6395ada64d2788e96a1066a4a0672305a0116
Luis Amez <luis.amez.vitoria@gmail.com>
2020-03-16 20:27:17 +0100

    Implement functionality for parseSandwichJson so the raw json data can be converted into a Sandwich object

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,57 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
+    private static final String NAME_KEY = "name";
+    private static final String MAIN_NAME_KEY = "mainName";
+    private static final String ALSO_KNOWN_AS_KEY = "alsoKnownAs";
+    private static final String PLACE_OF_ORIGIN_KEY = "placeOfOrigin";
+    private static final String DESCRIPTION_KEY = "description";
+    private static final String IMAGE_KEY = "image";
+    private static final String INGREDIENTS = "ingredients";
+
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sandwich;
+
+        try {
+            JSONObject sandwichJsonObject = new JSONObject(json);
+
+            JSONObject nameJsonObject = sandwichJsonObject.getJSONObject(NAME_KEY);
+
+            String mainName = nameJsonObject.getString(MAIN_NAME_KEY);
+
+            JSONArray alsoKnownAsJsonArray = nameJsonObject.getJSONArray(ALSO_KNOWN_AS_KEY);
+            List<String> alsoKnownAs = new ArrayList<>(alsoKnownAsJsonArray.length());
+            for (int i = 0; i < alsoKnownAsJsonArray.length(); i++) {
+                alsoKnownAs.add(alsoKnownAsJsonArray.getString(i));
+            }
+
+            String placeOfOrigin = sandwichJsonObject.getString(PLACE_OF_ORIGIN_KEY);
+
+            String description = sandwichJsonObject.getString(DESCRIPTION_KEY);
+
+            String image = sandwichJsonObject.getString(IMAGE_KEY);
+
+            JSONArray ingredientsJsonArray = sandwichJsonObject.getJSONArray(INGREDIENTS);
+            List<String> ingredients = new ArrayList<>(ingredientsJsonArray.length());
+            for (int i = 0; i < ingredientsJsonArray.length(); i++) {
+                ingredients.add(ingredientsJsonArray.getString(i));
+            }
+
+            sandwich = new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return sandwich;
     }
 }
